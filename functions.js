a8e9325930893ec24dc5daf4607d1439fe0be59a
selim2647 <mselimdalgic1@gmail.com>
2022-12-08 19:36:40 +0100

    Week-04-assignment-02 is tested.

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pArray) => {
+  let result = pArray[0];
+  let newArray = pArray[1].split(",");
+  for (let item = 0; item < newArray.length; item++) {
+    for (let element = 0; element < newArray.length; element++) {
+      if (newArray[item] + newArray[element] == result) {
+        return newArray[item] + "," + newArray[element];
+      }
+    }
+  }
+  return "not possible";
+};
 
 export { isTheWordSplit };
